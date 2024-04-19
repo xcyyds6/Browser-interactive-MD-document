623703339b7f958963e57e08d87b7a503f7eb28d
星辰 <2026093925@qq.com>
2024-04-19 15:20:01 +0800

    初始化

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,26 @@
+module.exports = {
+    title: '星辰的文档',
+    description: '文档是开发坚强的后盾！',
+    plugins:{
+        '@vuepress/active-header-links':{},
+        'autobar':{}
+    },
+    base:'/',
+    markdown: {
+        lineNumbers: true
+    },
+    themeConfig: {
+      nav: [
+        { text: '百度', link: 'https://www.baidu.com', target:'_self', rel:'' },
+        { text: 'Guide', link: '/guide/', target:'_blank' },
+        {
+          text: 'Languages',
+          ariaLabel: 'Language Menu',
+          items: [
+            { text: 'Chinese', link: '/language/chinese/' },
+            { text: 'Japanese', link: '/language/japanese/' }
+          ]
+        }
+      ]
+    }
+  }
